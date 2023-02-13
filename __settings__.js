b5d4f9660fab5b6bbff2c5274d31779eb7f239cc
InfinityLightCode <74560258+InfinityLightCode@users.noreply.github.com>
2023-02-12 16:59:11 -0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1659316.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 121043209, 121043210, 121043211, 121043212, 121209507, 121209583, 121210453, 121221870, 121226493, 121226496, 121226672 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/121178044/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/121178045/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/121178043/1/ammo.js', 'preload' : true},
+];
